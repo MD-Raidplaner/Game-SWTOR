01018c063e069cf041fb6c38c5557f62bca19614
xDerasier <deraiser@gmx.de>
2025-06-02 15:47:01 +0200

    Add classification support for SWTOR: register new classification items and update event handling

--- files_wcf/lib/bootstrap/de.md-raidplaner.game.swtor.php
+++ files_wcf/lib/bootstrap/de.md-raidplaner.game.swtor.php
@@ -3,6 +3,7 @@
 use rp\event\character\AvailableCharactersChecking;
 use rp\event\character\CharacterAddCreateForm;
 use rp\event\character\CharacterEditData;
+use rp\event\classification\ClassificationCollecting;
 use rp\event\event\EventCreateForm;
 use rp\event\faction\FactionCollecting;
 use rp\event\game\GameCollecting;
@@ -11,6 +12,7 @@ use rp\event\raid\AddAttendeesChecking;
 use rp\event\raid\character\CharacterCollecting;
 use rp\event\role\RoleCollecting;
 use rp\system\cache\eager\GameCache;
+use rp\system\classification\ClassificationItem;
 use rp\system\event\listener\SWTORAddAttendeesChecking;
 use rp\system\event\listener\SWTORAvailableCharactersChecking;
 use rp\system\event\listener\SWTORCharacterAddCreateFormListener;
@@ -143,5 +145,404 @@ return static function (): void {
         $event->register(new RoleItem('damagedealer', 'swtor'));
         $event->register(new RoleItem('heal', 'swtor'));
         $event->register(new RoleItem('tank', 'swtor'));
+    });
+
+    $eventHandler->register(ClassificationCollecting::class, static function (ClassificationCollecting $event) {
+        $event->register(new ClassificationItem(
+            'operative',
+            'swtor',
+            factions: [
+                'imperial',
+                'republic',
+            ],
+            races: [
+                'cathar',
+                'chiss',
+                'cyborg',
+                'human',
+                'miraluka',
+                'mirialan',
+                'rattataki',
+                'sith',
+                'togruta',
+                'twilek',
+                'zabrak',
+            ],
+            roles: [
+                'damagedealer',
+                'heal',
+            ],
+        ));
+        $event->register(new ClassificationItem(
+            'sniper',
+            'swtor',
+            factions: [
+                'imperial',
+                'republic',
+            ],
+            races: [
+                'cathar',
+                'chiss',
+                'cyborg',
+                'human',
+                'miraluka',
+                'mirialan',
+                'rattataki',
+                'sith',
+                'togruta',
+                'twilek',
+                'zabrak',
+            ],
+            roles: [
+                'damagedealer',
+            ],
+        ));
+        $event->register(new ClassificationItem(
+            'sorcerer',
+            'swtor',
+            factions: [
+                'imperial',
+                'republic',
+            ],
+            races: [
+                'cathar',
+                'chiss',
+                'cyborg',
+                'human',
+                'miraluka',
+                'mirialan',
+                'rattataki',
+                'sith',
+                'togruta',
+                'twilek',
+                'zabrak',
+            ],
+            roles: [
+                'damagedealer',
+                'heal',
+            ]
+        ));
+        $event->register(new ClassificationItem(
+            'assassin',
+            'swtor',
+            factions: [
+                'imperial',
+                'republic',
+            ],
+            races: [
+                'cathar',
+                'chiss',
+                'cyborg',
+                'human',
+                'miraluka',
+                'mirialan',
+                'rattataki',
+                'sith',
+                'togruta',
+                'twilek',
+                'zabrak',
+            ],
+            roles: [
+                'damagedealer',
+                'tank',
+            ]
+        ));
+        $event->register(new ClassificationItem(
+            'juggernaut',
+            'swtor',
+            factions: [
+                'imperial',
+                'republic',
+            ],
+            races: [
+                'cathar',
+                'chiss',
+                'cyborg',
+                'human',
+                'miraluka',
+                'mirialan',
+                'rattataki',
+                'sith',
+                'togruta',
+                'twilek',
+                'zabrak',
+            ],
+            roles: [
+                'damagedealer',
+                'tank',
+            ]
+        ));
+        $event->register(new ClassificationItem(
+            'commando',
+            'swtor',
+            factions: [
+                'imperial',
+                'republic',
+            ],
+            races: [
+                'cathar',
+                'chiss',
+                'cyborg',
+                'human',
+                'miraluka',
+                'mirialan',
+                'rattataki',
+                'sith',
+                'togruta',
+                'twilek',
+                'zabrak',
+            ],
+            roles: [
+                'damagedealer',
+                'heal',
+            ]
+        ));
+        $event->register(new ClassificationItem(
+            'mercenary',
+            'swtor',
+            factions: [
+                'imperial',
+                'republic',
+            ],
+            races: [
+                'cathar',
+                'chiss',
+                'cyborg',
+                'human',
+                'miraluka',
+                'mirialan',
+                'rattataki',
+                'sith',
+                'togruta',
+                'twilek',
+                'zabrak',
+            ],
+            roles: [
+                'damagedealer',
+                'heal',
+            ]
+        ));
+        $event->register(new ClassificationItem(
+            'vanguard',
+            'swtor',
+            factions: [
+                'imperial',
+                'republic',
+            ],
+            races: [
+                'cathar',
+                'chiss',
+                'cyborg',
+                'human',
+                'miraluka',
+                'mirialan',
+                'rattataki',
+                'sith',
+                'togruta',
+                'twilek',
+                'zabrak',
+            ],
+            roles: [
+                'damagedealer',
+                'tank',
+            ]
+        ));
+        $event->register(new ClassificationItem(
+            'powertech',
+            'swtor',
+            factions: [
+                'imperial',
+                'republic',
+            ],
+            races: [
+                'cathar',
+                'chiss',
+                'cyborg',
+                'human',
+                'miraluka',
+                'mirialan',
+                'rattataki',
+                'sith',
+                'togruta',
+                'twilek',
+                'zabrak',
+            ],
+            roles: [
+                'damagedealer',
+                'tank',
+            ]
+        ));
+        $event->register(new ClassificationItem(
+            'marauder',
+            'swtor',
+            factions: [
+                'imperial',
+                'republic',
+            ],
+            races: [
+                'cathar',
+                'chiss',
+                'cyborg',
+                'human',
+                'miraluka',
+                'mirialan',
+                'rattataki',
+                'sith',
+                'togruta',
+                'twilek',
+                'zabrak',
+            ],
+            roles: [
+                'damagedealer',
+            ]
+        ));
+        $event->register(new ClassificationItem(
+            'sentinel',
+            'swtor',
+            factions: [
+                'imperial',
+                'republic',
+            ],
+            races: [
+                'cathar',
+                'chiss',
+                'cyborg',
+                'human',
+                'miraluka',
+                'mirialan',
+                'rattataki',
+                'sith',
+                'togruta',
+                'twilek',
+                'zabrak',
+            ],
+            roles: [
+                'damagedealer',
+            ]
+        ));
+        $event->register(new ClassificationItem(
+            'guardian',
+            'swtor',
+            factions: [
+                'imperial',
+                'republic',
+            ],
+            races: [
+                'cathar',
+                'chiss',
+                'cyborg',
+                'human',
+                'miraluka',
+                'mirialan',
+                'rattataki',
+                'sith',
+                'togruta',
+                'twilek',
+                'zabrak',
+            ],
+            roles: [
+                'damagedealer',
+                'tank',
+            ]
+        ));
+        $event->register(new ClassificationItem(
+            'scoundrel',
+            'swtor',
+            factions: [
+                'imperial',
+                'republic',
+            ],
+            races: [
+                'cathar',
+                'chiss',
+                'cyborg',
+                'human',
+                'miraluka',
+                'mirialan',
+                'rattataki',
+                'sith',
+                'togruta',
+                'twilek',
+                'zabrak',
+            ],
+            roles: [
+                'damagedealer',
+                'heal',
+            ]
+        ));
+        $event->register(new ClassificationItem(
+            'sage',
+            'swtor',
+            factions: [
+                'imperial',
+                'republic',
+            ],
+            races: [
+                'cathar',
+                'chiss',
+                'cyborg',
+                'human',
+                'miraluka',
+                'mirialan',
+                'rattataki',
+                'sith',
+                'togruta',
+                'twilek',
+                'zabrak',
+            ],
+            roles: [
+                'damagedealer',
+                'heal',
+            ]
+        ));
+        $event->register(new ClassificationItem(
+            'shadow',
+            'swtor',
+            factions: [
+                'imperial',
+                'republic',
+            ],
+            races: [
+                'cathar',
+                'chiss',
+                'cyborg',
+                'human',
+                'miraluka',
+                'mirialan',
+                'rattataki',
+                'sith',
+                'togruta',
+                'twilek',
+                'zabrak',
+            ],
+            roles: [
+                'damagedealer',
+                'tank',
+            ]
+        ));
+        $event->register(new ClassificationItem(
+            'gunslinger',
+            'swtor',
+            factions: [
+                'imperial',
+                'republic',
+            ],
+            races: [
+                'cathar',
+                'chiss',
+                'cyborg',
+                'human',
+                'miraluka',
+                'mirialan',
+                'rattataki',
+                'sith',
+                'togruta',
+                'twilek',
+                'zabrak',
+            ],
+            roles: [
+                'damagedealer',
+            ]
+        ));
     });
 };
